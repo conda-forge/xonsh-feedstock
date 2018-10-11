7b50d6578149d4f13331fe0dd69ae72e129e4230
Marius van Niekerk <marius.v.niekerk@gmail.com>
2018-10-11 11:33:03 -0400

    MNT: Re-rendered with conda-smithy 3.1.12.post.dev19 and pinning 2018.10.11

--- .circleci/build_steps.sh
+++ .circleci/build_steps.sh
@@ -35,6 +35,8 @@ make_build_number "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
 conda build "${RECIPE_ROOT}" -m "${CI_SUPPORT}/${CONFIG}.yaml" \
     --clobber-file "${CI_SUPPORT}/clobber_${CONFIG}.yaml"  --quiet
 
+
 upload_package "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
+
 
 touch "/home/conda/feedstock_root/build_artifacts/conda-forge-build-done-${CONFIG}"
